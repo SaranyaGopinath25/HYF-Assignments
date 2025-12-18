c58159802386c8b355812f0f868a57292bc95f7b
Saranya Gopinath <142643776+SaranyaGopinath25@users.noreply.github.com>
2025-12-18 17:32:36 +0100

    Review changes done

--- courses/foundation/databases/tasks.sql
+++ courses/foundation/databases/tasks.sql
@@ -8,10 +8,10 @@ SELECT COUNT(*) from task t;
 SELECT COUNT(*) from task t where t.due_date IS NULL 
 
 -- 3. Find all the tasks that are marked as done.
-SELECT * from task t where t.status_id = 3;
+SELECT * from task t JOIN status s on t.status_id = s.id where s.name = 'Done';
 
 -- 4. Find all the tasks that are not marked as done.
-SELECT * from task t where t.status_id != 3;
+SELECT * from task t JOIN status s on t.status_id = s.id where s.name != 'Done';
 
 -- 5. Get all the tasks, sorted with the most recently created first.
 SELECT * from task t ORDER BY created DESC 
@@ -20,23 +20,14 @@ SELECT * from task t ORDER BY created DESC
 SELECT * from task t ORDER BY created DESC LIMIT 1
 
 -- 7. Get the title and due date of all tasks where the title or description contains database.
-SELECT title, t.due_date  from task t where t.title LIKE "%database%" OR t.description LIKE  "%database%";
+SELECT t.title, t.due_date  from task t where LOWER(t.title) LIKE "%database%" OR LOWER(t.description) LIKE  "%database%";
 
 -- 8. Get the title and status (as text) of all tasks.
-SELECT title, CASE status_id when 1 then "New Task" when 2 then "In Progress" when 3 then "Completed" END AS status from task t 	
+SELECT t.title, s.name as status FROM task t JOIN status s on t.status_id =s.id;
 
 -- 9. Get the name of each status, along with a count of how many tasks have that status.
---Step 1 -  Select distinct status 
-SELECT DISTINCT CASE status_id when 1 then "New Task" when 2 then "In Progress" when 3 then "Completed" END AS status from task t 
-
---Step 2 - Count the number of status
-SELECT COUNT (*) from task t where t.status_id IN (2);
-
-SELECT DISTINCT CASE status_id when 1 then "New Task" when 2 then "In Progress" when 3 then "Completed" END AS status, COUNT(*) AS tasks_count from task t GROUP BY status 
-
+SELECT s.name as statusName, COUNT(t.status_id ) as NumOfTasks from status s JOIN task t on s.id = t.status_id GROUP BY s.name;
 
 -- 10. Get the names of all statuses, sorted by the status with most tasks first.
-SELECT DISTINCT CASE status_id when 1 then "New Task" when 2 then "In Progress" when 3 then "Completed" END AS status, COUNT(*) AS tasks_count from task t GROUP BY status ORDER BY tasks_count DESC 
-
-
+SELECT s.name as statusName, COUNT(t.status_id ) as NumOfTasks from status s JOIN task t on s.id = t.status_id GROUP BY s.name ORDER BY NumOfTasks DESC 
 
