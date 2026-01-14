183dc55818538ab2cd2ec1e38fc66e359541a927
Saranya Gopinath <142643776+SaranyaGopinath25@users.noreply.github.com>
2026-01-14 21:13:45 +0100

    Hogwarts house & hfyBay assignments done

--- courses/foundation/intro-to-frontend/HYFBay/main.js
+++ courses/foundation/intro-to-frontend/HYFBay/main.js
@@ -3,9 +3,55 @@ console.log("Script loaded");
 const products = getAvailableProducts();
 console.log(products);
 
-// This should create the ul and the li's with the individual products details
+const $ = (selector) => document.querySelector.bind(document)(selector);
+const createEle = (element) => document.createElement(element);
+
+const searchInput = createEle("input");
+searchInput.type = "text";
+searchInput.id = "search-input";
+searchInput.placeholder = "Search product...";
+$("header").insertBefore(searchInput, $("header nav"));
+
+searchInput.addEventListener("input", function (e){searchProduct(e)});
+
+const productLink = $("a[href='#prod']");
+productLink.addEventListener("click", renderProducts.bind(null, products));
+
+function searchProduct(e)
+{
+  const searchTerm = e.target.value.toLowerCase();
+  const searchResult = products.filter(product => product.name.toLowerCase().includes(searchTerm));
+  $("main").innerHTML = "";
+  renderProducts(searchResult);
+  
+}
+  // This should create the ul and the li's with the individual products details
 function renderProducts(products) {
   // your code goes here
+  const main = $("main");
+  const ul = createEle("ul");
+  for (const product of products) {
+    // product card
+    const card = createEle("div");
+    card.classList.add("product-item");
+    ul.appendChild(card);
+
+    // product name
+    const name = createEle("h2");
+    name.textContent = product.name;
+    card.appendChild(name);
+
+    // product price
+    const price = createEle("p");
+    price.textContent = `Price: $${product.price}`;
+    card.appendChild(price);
+
+    // product rating
+    const rating = createEle("p");
+    rating.textContent = `Rating: ${product.rating} / 10`;
+    card.appendChild(rating);
+  }
+  main.appendChild(ul);
 }
 
 renderProducts(products); 
