17a5e59fac19b5509fb92d25625f72012759395b
Saranya Gopinath <142643776+SaranyaGopinath25@users.noreply.github.com>
2026-04-17 00:50:51 +0200

    UI for all the 3 screens Login, Dashboard and Screenshot preview implemented

---
+++ courses/frontend/advanced-javascript/week-4/Screenshot-app/views/ScreenshotView.js
@@ -0,0 +1,61 @@
+
+class ScreenshotView {
+    render(){
+        const screenshotDivElement = document.createElement('div');
+        screenshotDivElement.classList.add('screenshot-view');
+
+        const titleElement = document.createElement('h1');
+        titleElement.textContent = 'Screenshot 📸';
+
+        const messageElement = document.createElement('p');
+        messageElement.textContent = 'Your screenshot will appear here!';
+
+        const previewDivElement = document.createElement('div');
+        previewDivElement.classList.add('preview-div');
+
+        const loadingBarContainer = document.createElement('div');
+        loadingBarContainer.classList.add('loading-bar-container');
+        loadingBarContainer.setAttribute('id', 'loading-bar-container');
+        loadingBarContainer.style.display = 'none';
+        
+        const loadingBar = document.createElement('div');
+        loadingBar.classList.add('loading-bar');
+        loadingBar.setAttribute('id', 'loading-bar');
+        loadingBar.style.width = '0%';
+        loadingBarContainer.appendChild(loadingBar);
+        previewDivElement.appendChild(loadingBarContainer);
+
+        const previewImage = document.createElement('img');
+        previewImage.setAttribute('id', 'preview-image');
+        previewDivElement.appendChild(previewImage);
+
+        const buttonDivElement = document.createElement('div');
+        buttonDivElement.classList.add('button-div');
+        
+        const backBtn = document.createElement('button');
+        backBtn.classList.add('btn');
+        backBtn.setAttribute('id', 'back-btn');
+        backBtn.textContent = "Take Another Screenshot";
+        backBtn.type = 'submit';
+        buttonDivElement.appendChild(backBtn);
+
+        const saveBtn = document.createElement('button');
+        saveBtn.classList.add('btn');
+        saveBtn.setAttribute('id', 'save-btn');
+        saveBtn.textContent = "Save Screenshot";
+        saveBtn.type = 'submit';
+        saveBtn.disabled = true;
+        buttonDivElement.appendChild(saveBtn);
+
+
+        previewDivElement.appendChild(buttonDivElement);
+
+        screenshotDivElement.appendChild(titleElement);
+        screenshotDivElement.appendChild(messageElement);
+        screenshotDivElement.appendChild(previewDivElement);
+
+        return screenshotDivElement;
+    }
+}
+
+export default ScreenshotView;
