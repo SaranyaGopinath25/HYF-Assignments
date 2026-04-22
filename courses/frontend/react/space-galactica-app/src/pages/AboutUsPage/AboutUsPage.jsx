a5f9062e67d247cd65fd536238e1e571ef17623c
Saranya Gopinath <142643776+SaranyaGopinath25@users.noreply.github.com>
2026-04-22 18:51:26 +0200

    Crew section added in the About-us page

--- courses/frontend/react/space-galactica-app/src/pages/AboutUsPage/AboutUsPage.jsx
+++ courses/frontend/react/space-galactica-app/src/pages/AboutUsPage/AboutUsPage.jsx
@@ -1,40 +1,43 @@
 import styles from './AboutUsPage.module.css';
+import OurCrew from './OurCrew.jsx';
+import OurValues from './OurValues.jsx';
+import OurPartners from './OurPartners.jsx';
 
 // 🧑🏽‍🚀 Task - Week 1
 // After you are finished with creating the page, move the OurValues, OurCrew, OurPartners components into their own files in this folder.
 // Import and use the components from the newly created files.
 
-const OurValues = () => {
+// const OurValues = () => {
   // 🧑🏽‍🚀 Task - Week 1
   // Create the "Our Values" section.
   // Use the descriptions provided in /src/pages/AboutUsPage/README.md.
   // Some inspiration ideas can be found in /data/inspiration_about_us.
-  return (
-    <p> ADD OUR VALUES HERE </p>
-  );
-};
+  // return (
+    // <p> ADD OUR VALUES HERE </p>
+  // );
+// };
 
-const OurCrew = () => {
+// const OurCrew = () => {
   // 🧑🏽‍🚀 Task - Week 1
   // Create the "Our Crew section".
   // Use the descriptions provided in /src/pages/AboutUsPage/README.md.
   // Use the pictures from /public/crew.
   // Some inspiration ideas can be found in /data/inspiration_about_us.
-  return (
-    <p> ADD OUR CREW HERE </p>
-  );
-}
+  // return (
+    // <OurCrew />
+  // );
+// }
 
-const OurPartners = () => {
+// const OurPartners = () => {
   // 🧑🏽‍🚀 Task - Week 1
   // Create the "Our Partners section".
   // Use the descriptions provided in /src/pages/AboutUsPage/README.md.
   // Use the pictures from /public/business_partners.
   // Some inspiration ideas can be found in /data/inspiration_about_us.
-  return (
-    <p> ADD OUR PARTNERS HERE </p>
-  );
-}
+  // return (
+    // <p> ADD OUR PARTNERS HERE </p>
+  // );
+// }
 
 
 export const Crew = () => {
@@ -43,12 +46,13 @@ export const Crew = () => {
       <main className="mainContent">
         <h1>About us</h1>
         <section className="card">
-          <h2>Our Values</h2>
-          <OurValues/>
+          <OurCrew/>
         </section>
         <section className="card">
-          <h2>The crew</h2>
-          <OurCrew/>
+          <OurValues/>
+        </section>
+        <section className='card'>
+          <OurPartners />
         </section>
 
          {/* 🧑🏽‍🚀 Task - Week 1 */}
